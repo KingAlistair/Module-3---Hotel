46ccae11003b942be5e693f05c3109b1bc0a8c76
BalazsGitHub <74620767+BalazsGitHub@users.noreply.github.com>
2021-11-15 11:14:02 +0100

    Add files via upload
    Guest class added

--- HotelApplication/src/DataBase.java
+++ HotelApplication/src/DataBase.java
@@ -2,14 +2,6 @@ import java.util.ArrayList;
 
 public class DataBase {
 
-    private ArrayList<Staff> staffList;
-   // private Arraylist <Guest> guestList;
-   // private Arraylist <Room> guestList;
-
-    public DataBase(){
-        this.staffList = getStaffList();
-    }
-
     public static ArrayList<Staff> getStaffList(){
         ArrayList<Staff> staffList = new ArrayList<>();
         Staff staff1 = new Staff("Fé","Santa", "the cleaning lady", 19999, "+55123456789");
