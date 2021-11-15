9d1b175a672398c92f18684625418721f0bf52fe
mubibic <amal487d@stud.kea.dk>
2021-11-15 15:24:28 +0100

    database objects

--- HotelApplication/src/Menu.java
+++ HotelApplication/src/Menu.java
@@ -6,10 +6,10 @@ public class Menu {
     public void mainMenu() {
       DataBase database = new DataBase();
 
-        System.out.println("1 Manage Booking");
-        System.out.println("2 Manage Staff ");
-        System.out.println("3 Manage Guests");
-        System.out.println("4 Manage Room");
+        System.out.println("1 Administer Booking");
+        System.out.println("2 Administer Staff ");
+        System.out.println("3 Administer Guests");
+        System.out.println("4 Administer Room");
         System.out.println("Press \"r\" to reset Database");
         System.out.println("Press 5 to quit");
         Scanner scanner = new Scanner(System.in);
