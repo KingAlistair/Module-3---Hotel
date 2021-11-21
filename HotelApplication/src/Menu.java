60c178680980d0369846a98914dd3667da15a95b
BalazsGitHub <balazs.dombovari@gmail.com>
2021-11-21 21:42:54 +0100

    Just needed a break

--- HotelApplication/src/Menu.java
+++ HotelApplication/src/Menu.java
@@ -32,6 +32,7 @@ public class Menu {
 
             case "3":
                 administerGuest();
+                break;
 
             case "r":
                 DataBase dataBase = new DataBase();
